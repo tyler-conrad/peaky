ba8845c78c9e65164a6a37429985ec69cb9a3667
Tyler Conrad <conradt80@gmail.com>
2013-10-05 21:19:52 -0500

    Parse ansi output from top.

--- src/peaky.py
+++ src/peaky.py
@@ -4,7 +4,7 @@
 '''
 Usage: peaky.py [options]
 Options:
-  -f, --fast     Act quickly
+  -l, --lines=   Max number of lines output from top. [default: 1024]
       --version  Display Twisted version and exit.
       --help     Display this help and exit.
 '''
@@ -12,14 +12,19 @@ Options:
 from sys import argv
 from os import environ
 
+from twisted.internet import reactor
+from twisted.internet.protocol import ProcessProtocol
 from twisted.python.usage import Options
 from twisted.python.usage import UsageError
-from twisted.internet.protocol import ProcessProtocol
-from twisted.internet import reactor
+from twisted.conch.ui.ansi import AnsiParser
+from twisted.conch.ui.ansi import ColorText
 
 class Options(Options):
-    optFlags = [
-        ["fast", "f", "Act quickly"]]
+#     optFlags = [
+#         ["fast", "f", "Act quickly"]]
+
+    optParameters = [
+        ['lines', 'l', 1024, 'Max number of lines output from top.', int]]
 
 opts = Options()
 try:
@@ -29,20 +34,40 @@ except UsageError as ue:
     print '%s: Try --help for usage details.' % (argv[0])
     exit(1)
 
-if opts['fast']:
-    print "fast",
+class PeakyAnsiParser(AnsiParser, object):
+    def __init__(
+            self,
+            defaultFG=ColorText.WHITE,
+            defaultBG=ColorText.BLACK):
+        super(PeakyAnsiParser, self).__init__(
+            defaultFG,
+            defaultBG)
+        self.color_text_list = []
+
+    def writeString(self, color_text):
+        text = color_text.text = color_text.text.replace('\r\n', '')
+        if not text or text == 'B':
+            return
+
+        self.color_text_list.append(color_text)
 
 class Top(ProcessProtocol):
     def __init__(self):
-        self.data = ""
+        self.data = ''
 
     def connectionMade(self):
-#         self.transport.write(top)
-        self.transport.closeStdin() # tell them we're done
+        pass
 
     def outReceived(self, data):
-        print "outReceived! with %d bytes!" % len(data)
-        self.data = self.data + data
+        if 'top -' in data:
+            def color_text_list():
+                pap = PeakyAnsiParser()
+                pap.parseString(self.data)
+                return pap.color_text_list
+
+            self.data = data
+        else:
+            self.data += data
 
     def errReceived(self, data):
         print "errReceived! with %d bytes!" % len(data)
@@ -53,7 +78,6 @@ class Top(ProcessProtocol):
 
     def outConnectionLost(self):
         print "outConnectionLost! The child closed their stdout!"
-        print self.data
 
     def errConnectionLost(self):
         print "errConnectionLost! The child closed their stderr."
@@ -66,10 +90,23 @@ class Top(ProcessProtocol):
         print "quitting"
         reactor.stop()
 
+def env_term():
+    try:
+        return environ['TERM']
+    except KeyError:
+        print 'TERM environment variable not set.'
+        return None
+        
+
 reactor.spawnProcess(
     processProtocol=Top(),
-    executable="top",
-    args=["top"],
-    env={'TERM': environ['TERM']},
+    executable='top',
+    args=['top'],
+    env={
+        'TERM': environ['TERM'],
+        'COLUMNS': '512',
+        'LINES': str(opts['lines']),
+    },
     usePTY=1)
+
 reactor.run()
