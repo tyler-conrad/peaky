325ebf6920bab38ea3354d7362183d27f5ff98fe
Tyler Conrad <conradt80@gmail.com>
2013-10-05 23:23:27 -0500

    Setup kivy.

--- src/peaky.py
+++ src/peaky.py
@@ -12,27 +12,75 @@ Options:
 from sys import argv
 from os import environ
 
-from twisted.internet import reactor
 from twisted.internet.protocol import ProcessProtocol
 from twisted.python.usage import Options
 from twisted.python.usage import UsageError
 from twisted.conch.ui.ansi import AnsiParser
 from twisted.conch.ui.ansi import ColorText
 
+from kivy.app import App
+from kivy.support import install_twisted_reactor
+from kivy.utils import QueryDict
+from kivy.interactive import InteractiveLauncher
+
 class Options(Options):
-#     optFlags = [
-#         ["fast", "f", "Act quickly"]]
+    optFlags = [
+        ['interactive', 'i', 'Launch in interactive mode.']]
 
     optParameters = [
         ['lines', 'l', 1024, 'Max number of lines output from top.', int]]
 
-opts = Options()
-try:
-    opts.parseOptions(argv[1:])
-except UsageError as ue:
-    print '%s: %s' % (argv[0], ue)
-    print '%s: Try --help for usage details.' % (argv[0])
-    exit(1)
+class LoaderBase(object):
+    def parse_opts(self, opt_parser):
+        opt_parser.parseOptions(argv[1:])
+
+    def get_opt(self):
+        opt_parser = Options()
+        self.parse_opts(opt_parser)
+        return QueryDict(opt_parser)
+
+    def term(self):
+        return environ['TERM']
+
+    def spawn_top(self, opt):
+        from twisted.internet import reactor
+        reactor.spawnProcess(
+            processProtocol=TopProc(),
+            executable='top',
+            args=['top'],
+            env={
+                'TERM': environ['TERM'],
+                'COLUMNS': '512',
+                'LINES': str(opt.lines)},
+            usePTY=1)
+
+    def load(self):
+        app = Peaky(kv_file='peaky.kv')
+        opt = self.get_opt()
+        if opt.interactive:
+            InteractiveLauncher(app).run()
+        else:
+            app.run()
+
+class LoaderErr(LoaderBase):
+    def parse_opts(self, opt_parser):
+        try:
+            opt = super(LoaderErr, self).parse_opts(opt_parser)
+        except UsageError as ue:
+            print '%s: %s' % (argv[0], ue)
+            print '%s: Try --help for usage details.' % (argv[0])
+            exit(1)
+        return opt
+
+    def term(self):
+        try:
+            term = super(LoaderErr, self).env_term()
+        except KeyError:
+            print 'TERM environment variable not set.'
+        return term
+
+class Loader(LoaderErr):
+    pass
 
 class PeakyAnsiParser(AnsiParser, object):
     def __init__(
@@ -48,10 +96,9 @@ class PeakyAnsiParser(AnsiParser, object):
         text = color_text.text = color_text.text.replace('\r\n', '')
         if not text or text == 'B':
             return
-
         self.color_text_list.append(color_text)
 
-class Top(ProcessProtocol):
+class TopProc(ProcessProtocol):
     def __init__(self):
         self.data = ''
 
@@ -88,25 +135,10 @@ class Top(ProcessProtocol):
     def processEnded(self, reason):
         print "processEnded, status %d" % (reason.value.exitCode,)
         print "quitting"
-        reactor.stop()
 
-def env_term():
-    try:
-        return environ['TERM']
-    except KeyError:
-        print 'TERM environment variable not set.'
-        return None
-        
+class Peaky(App):
+    def __init__(self, **kw):
+        super(Peaky, self).__init__(**kw)
+        install_twisted_reactor()
 
-reactor.spawnProcess(
-    processProtocol=Top(),
-    executable='top',
-    args=['top'],
-    env={
-        'TERM': environ['TERM'],
-        'COLUMNS': '512',
-        'LINES': str(opts['lines']),
-    },
-    usePTY=1)
-
-reactor.run()
+Loader().load()
