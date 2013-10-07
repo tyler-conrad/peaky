c4f2796c895d15ed7d7cbdf1e91ae0049430d81c
Tyler Conrad <conradt80@gmail.com>
2013-10-06 19:10:57 -0500

    Update ArgSplitter test.

--- test/test_arg_split.py
+++ test/test_arg_split.py
@@ -23,6 +23,28 @@ def test_no_args(monkeypatch):
     with pytest.raises(IndexError):
         splitter.peaky_args()
 
+def test_no_args_with_seperators(monkeypatch):
+    patch_argv(monkeypatch, ['src/peaky.py', '--', '--'])
+
+    from arg_split import ArgSplitter
+    splitter = ArgSplitter()
+
+    assert splitter.non_kivy_args() == ['--']
+    assert splitter.kivy_args() == ['src/peaky.py']
+    assert splitter.top_args() == []
+    assert splitter.peaky_args() == ['src/peaky.py']
+
+def test_no_args_extra_separators(monkeypatch):
+    patch_argv(monkeypatch, ['src/peaky.py', '--', '--', '--'])
+
+    from arg_split import ArgSplitter
+    splitter = ArgSplitter()
+
+    assert splitter.non_kivy_args() == ['--', '--']
+    assert splitter.kivy_args() == ['src/peaky.py']
+    assert splitter.top_args() == []
+    assert splitter.peaky_args() == ['src/peaky.py', '--']
+
 def test_one_arg(monkeypatch):
     arg_list = ['src/peaky.py']
     patch_argv(monkeypatch, arg_list) 
@@ -34,4 +56,88 @@ def test_one_arg(monkeypatch):
     assert splitter.kivy_args() == arg_list
     assert splitter.top_args() == []
     assert splitter.peaky_args() == arg_list
+
+def test_fancy_args(monkeypatch):
+    arg_list = [
+        'src/peaky.py',
+        '-h',
+        '--debug',
+        '--auto-fullscreen',
+        '--config', 'kivy:desktop:1',
+        '-c', 'kivy:exit_on_escape:1',
+        '-f',
+        '--fake-fullscreen',
+        '-w',
+        '--provider', 'ccvtable1:tuio,192.168.0.1:3333',
+        '-m', 'list',
+        '--module=inspector',
+        '-m', 'a,b,c',
+        '--module=a,b,c',
+        '--rotation=90',
+        '-r', '90',
+        '-s',
+        '--size=640x480',
+        '--dpi=96',
+        '--',
+        '-hv',
+        '-bcHiSs',
+        '-d', '34.34',
+        '-n', '4848',
+        '-U', 'user',
+        '-p', '85858,84848',
+        '-w', '8484',
+        '--',
+        '-i',
+        '--help']
+    patch_argv(monkeypatch, arg_list) 
+
+    from arg_split import ArgSplitter
+    splitter = ArgSplitter()
+
+    assert splitter.non_kivy_args() == [
+        '-hv',
+        '-bcHiSs',
+        '-d', '34.34',
+        '-n', '4848',
+        '-U', 'user',
+        '-p', '85858,84848',
+        '-w', '8484',
+        '--',
+        '-i',
+        '--help']
+
+    assert splitter.kivy_args() == [
+        'src/peaky.py',
+        '-h',
+        '--debug',
+        '--auto-fullscreen',
+        '--config', 'kivy:desktop:1',
+        '-c', 'kivy:exit_on_escape:1',
+        '-f',
+        '--fake-fullscreen',
+        '-w',
+        '--provider', 'ccvtable1:tuio,192.168.0.1:3333',
+        '-m', 'list',
+        '--module=inspector',
+        '-m', 'a,b,c',
+        '--module=a,b,c',
+        '--rotation=90',
+        '-r', '90',
+        '-s',
+        '--size=640x480',
+        '--dpi=96']
+
+    assert splitter.top_args() == [
+        '-hv',
+        '-bcHiSs',
+        '-d', '34.34',
+        '-n', '4848',
+        '-U', 'user',
+        '-p', '85858,84848',
+        '-w', '8484']
+
+    assert splitter.peaky_args() == [
+        'src/peaky.py',
+        '-i',
+        '--help']
 
