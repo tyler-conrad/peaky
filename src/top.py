139fc088e0654b333f9889a411f8e00d9db999e1
Tyler Conrad <conradt80@gmail.com>
2013-10-06 21:21:46 -0500

    Exit peaky on invalid top option.

--- src/top.py
+++ src/top.py
@@ -8,6 +8,15 @@ from twisted.conch.ui.ansi import AnsiParser
 from twisted.conch.ui.ansi import ColorText
 from twisted.internet.protocol import ProcessProtocol
 
+from kivy.base import stopTouchApp
+
+def stop_peaky(exit_code=0):
+    stopTouchApp()
+    try:
+        exit(exit_code)
+    except:
+        pass
+
 class PeakyAnsiParser(AnsiParser, object):
     def __init__(
             self,
@@ -32,12 +41,14 @@ class TopProc(ProcessProtocol):
         pass
 
     def outReceived(self, data):
+        if 'top:' in data:
+            stop_peaky(1)
+
         if 'top -' in data:
             def color_text_list():
                 pap = PeakyAnsiParser()
                 pap.parseString(self.data)
                 return pap.color_text_list
-
             self.data = data
         else:
             self.data += data
@@ -57,7 +68,9 @@ class TopProc(ProcessProtocol):
 
     def processExited(self, reason):
         print "processExited, status %d" % (reason.value.exitCode,)
+        print reason.getErrorMessage()
 
     def processEnded(self, reason):
         print "processEnded, status %d" % (reason.value.exitCode,)
         print "quitting"
+        print reason.getErrorMessage()
